f6f3503be35905128c295d1833fbcf8224ac89e4
Sarah COUDERT <scoudert@e1r7p1.42.fr>
2015-06-05 14:45:36 +0200

    quick commit

--- ft_atof.c
+++ ft_atof.c
@@ -10,67 +10,97 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "computer.h"
 
-double		ft_atof(char *str, int *i)
+float		ft_atof(const char *str)
 {
-	double	nbr;
-	double	vir;
-	int		sign;
-	int		t;
+	float	n;
+	int		i;
+	int		k;
+	int		signe;
 
-	sign = 1;
-	nbr = 0.0;
-	vir = 0.0;
-	if (str[*i] == '-')
+	i = 0;
+	n = 0;
+	k = 0;
+	signe = 1;
+	if (!str)
+		return (0);
+	while (str[i] == ' ' || str[i] == '\v' || str[i] == '\t' || str[i] == '\r'
+			|| str[i] == '\f' || str[i] == '\n')
+		i++;
+	signe = (str[i] == '-') ? -signe : signe;
+	i = (str[i] == '-' || str[i] == '+') ? i + 1 : i;
+	while (str[i] > 47 && str[i] < 58)
 	{
-		*i = *i + 1;
-		sign = -1;
+		n = n * 10 + (str[i++] - 48);
+		if (str[i] == '.')
+			k = i++;
 	}
-	while (str[*i] != '\0' && ft_isdigit(str[*i]))
-	{
-		nbr = (nbr * 10) + str[*i] - '0';
-		*i = *i + 1;
-	}
-	if (str[*i] == '.' || str[*i] == ',')
-	{
-		*i = *i + 1;
-		t = *i;
-		while (ft_isdigit(str[t + *i]) && t <= *i + 7)
-			t++;
-		while (str[--t] != ',' && str[t] != '.')
-		{
-			vir = (vir / 10) + str[t] - '0';
-			*i = *i + 1;
-		}
-		vir = vir / 10;
-		*i = *i + 1;
-	}
-	return (sign * (nbr + vir));
+	while (k != 0 && str[++k])
+		signe = signe * 10;
+	return (n / signe);
 }
 
-void	ft_putdouble(double nbr)
-{
-	ft_putnbr(nbr);
-	nbr = nbr - (nbr / 1);
-	if (nbr < 1 && nbr > 0)
-	{
-			ft_putchar('.');
-	}
-}
-/*
-void	ft_putdouble(double nbr)
-{
-	int		t;
-	int		v;
+// #include "computer.h"
 
-	v = 1;
-	while (nbr / v >= 1)
-		v = v * 10;
-	while (nbr >= 1)
-	{
-		ft_putchar(nbr / v);
-		v = v / 10;
-		nbr = nbr / 10;
-	}
-}*/
+// double		ft_atof(char *str, int *i)
+// {
+// 	double	nbr;
+// 	double	vir;
+// 	int		sign;
+// 	int		t;
+
+// 	sign = 1;
+// 	nbr = 0.0;
+// 	vir = 0.0;
+// 	if (str[*i] == '-')
+// 	{
+// 		*i = *i + 1;
+// 		sign = -1;
+// 	}
+// 	while (str[*i] != '\0' && ft_isdigit(str[*i]))
+// 	{
+// 		nbr = (nbr * 10) + str[*i] - '0';
+// 		*i = *i + 1;
+// 	}
+// 	if (str[*i] == '.' || str[*i] == ',')
+// 	{
+// 		*i = *i + 1;
+// 		t = *i;
+// 		while (ft_isdigit(str[t + *i]) && t <= *i + 7)
+// 			t++;
+// 		while (str[--t] != ',' && str[t] != '.')
+// 		{
+// 			vir = (vir / 10) + str[t] - '0';
+// 			*i = *i + 1;
+// 		}
+// 		vir = vir / 10;
+// 		*i = *i + 1;
+// 	}
+// 	return (sign * (nbr + vir));
+// }
+
+// void	ft_putdouble(double nbr)
+// {
+// 	ft_putnbr(nbr);
+// 	nbr = nbr - (nbr / 1);
+// 	if (nbr < 1 && nbr > 0)
+// 	{
+// 			ft_putchar('.');
+// 	}
+// }
+// /*
+// void	ft_putdouble(double nbr)
+// {
+// 	int		t;
+// 	int		v;
+
+// 	v = 1;
+// 	while (nbr / v >= 1)
+// 		v = v * 10;
+// 	while (nbr >= 1)
+// 	{
+// 		ft_putchar(nbr / v);
+// 		v = v / 10;
+// 		nbr = nbr / 10;
+// 	}
+// }*/
