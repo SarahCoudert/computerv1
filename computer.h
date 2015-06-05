ec481b08ceea55bfede72ea3338d9889cd7e3808
Sarah COUDERT <scoudert@e1r7p1.42.fr>
2015-06-05 12:50:21 +0200

    quick commit

--- computer.h
+++ computer.h
@@ -1,11 +1,10 @@
 #ifndef COMPUTER_H
 # define COMPUTER_H
 
-#include "libft/includes/libft.h"
-#include <stdio.h>
-#define STRU		((t_stru*)(list->content))
-#define STRU_NEXT	((t_stru*)(list->next->content))
-
+# include "libft/includes/libft.h"
+# include <stdio.h>
+# define STRU		((t_stru*)(list->content))
+# define STRU_NEXT	((t_stru*)(list->next->content))
 
 typedef struct		s_stru
 {
@@ -14,14 +13,14 @@ typedef struct		s_stru
 	int				exp;
 }					t_stru;
 
-double			ft_atof(char *str, int *i);
-t_list			*parse(char *s);
-t_stru			*fill_stru(int *i, t_stru *stru, char *s);
-int				ft_skip(char *s, int *i, int b);
-t_list			*sort_list(t_list *list);
-double			square_root(double n);
-t_list			*add_list(t_list *list);
-void			form_reduit(t_list *list);
-void			print_res(double a, double b, double delta);
+double				ft_atof(char *str, int *i);
+t_list				*parse(char *s);
+t_stru				*fill_stru(int *i, t_stru *stru, char *s);
+int					ft_skip(char *s, int *i, int b);
+t_list				*sort_list(t_list *list);
+double				square_root(double n);
+t_list				*add_list(t_list *list);
+void				form_reduit(t_list *list);
+void				print_res(double a, double b, double delta);
 
 #endif
